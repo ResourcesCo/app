cd4f92f42076e23eff33023f461cc1564830cb96
Benjamin Atkin <ben@resources.co>
2021-02-17 23:37:07 -0500

    refactor: use UUID v4 for ids 📦

--- api/db/migrations/20210218043549_initial/migration.sql
+++ api/db/migrations/20210218043549_initial/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
-CREATE TABLE "Profile" (
-    "id" TEXT NOT NULL,
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "bot" BOOLEAN NOT NULL,
@@ -10,11 +10,12 @@ CREATE TABLE "Profile" (
 
 -- CreateTable
 CREATE TABLE "Page" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "path" TEXT NOT NULL,
     "body" TEXT NOT NULL,
     "metadata" JSONB NOT NULL,
+    "computed" JSONB NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY ("id")
@@ -22,21 +23,22 @@ CREATE TABLE "Page" (
 
 -- CreateTable
 CREATE TABLE "Action" (
-    "id" TEXT NOT NULL,
-    "authorId" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "name" TEXT NOT NULL,
     "path" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "body" TEXT NOT NULL,
     "metadata" JSONB NOT NULL,
+    "computed" JSONB NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "pageId" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+    "pageId" UUID NOT NULL,
 
     PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Profile.email_unique" ON "Profile"("email");
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Page.path_unique" ON "Page"("path");
@@ -45,7 +47,7 @@ CREATE UNIQUE INDEX "Page.path_unique" ON "Page"("path");
 CREATE INDEX "Page.name_index" ON "Page"("name");
 
 -- AddForeignKey
-ALTER TABLE "Action" ADD FOREIGN KEY ("authorId") REFERENCES "Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Action" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Action" ADD FOREIGN KEY ("pageId") REFERENCES "Page"("id") ON DELETE CASCADE ON UPDATE CASCADE;

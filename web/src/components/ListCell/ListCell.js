5690a7e505f88eac18d516c8eb429d6a8edcabb9
Benjamin Atkin <ben@resources.co>
2021-02-19 23:18:08 -0500

    fix: add key to markup & use only one link component 🌐

--- web/src/components/ListCell/ListCell.js
+++ web/src/components/ListCell/ListCell.js
@@ -1,10 +1,11 @@
 /** @jsx jsx */
 import { Link, routes } from '@redwoodjs/router'
-import { jsx, Box, Text, Link as ThemeLink, Container } from 'theme-ui'
+import { jsx, Box, Text, Container } from 'theme-ui'
 
 export const QUERY = gql`
   query ListQuery {
     pages {
+      id
       name
       path
     }
@@ -18,22 +19,18 @@ export const Empty = () => <div>Empty</div>
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
 export const Success = ({ pages }) => {
-  return (pages.map(({ name, path }) => (
-    <Container sx={{ maxWidth: 768, p: 2, pt: 4, mx: 'auto' }}>
+  return pages.map(({ id, name, path }) => (
+    <Container key={id} sx={{ maxWidth: 768, p: 2, pt: 4, mx: 'auto' }}>
       <Box>
         <Box>
-          <Link to={routes.view({ path })}>
-            <ThemeLink>
-              {name}
-            </ThemeLink>
+          <Link to={routes.view({ path })} sx={{ color: 'primary' }}>
+            {name}
           </Link>
         </Box>
         <Box>
-          <Text sx={{ fontSize: 1, color: 'gray' }}>
-            {path}
-          </Text>
+          <Text sx={{ fontSize: 1, color: 'gray' }}>{path}</Text>
         </Box>
       </Box>
     </Container>
-  )))
+  ))
 }
